89c36061940c024b12f72c9d4d5ac732e056f8d7
Alex <ascott.sco@gmail.com>
2023-06-23 13:46:52 +0100

    Temperature converter reversed conversion

--- tempconverter.c
+++ tempconverter.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /* print Fahrenheit-Celsius table
- * for fahr = 0, 20, ..., 300; 
+ * for fahr = 0, 20, ..., 300;
  * using floating point arithmetic*/
 
 /*
-int main() 
+int main()
 {
   float fahr, celsius;
   int lower, upper, step;
@@ -15,7 +15,7 @@ int main()
   step = 20;
 
   fahr = lower;
-  
+
   printf("%3s \t %6s \n","Fahr","Celsius");
 
   while (fahr <= upper)
@@ -27,8 +27,7 @@ int main()
 }
 */
 
-int main() 
-{
+int main() {
   float fahr, celsius;
   int lower, upper, step;
 
@@ -36,14 +35,13 @@ int main()
   upper = 300;
   step = 20;
 
-  fahr = lower;
-  
-  printf("%3s \t %6s \n","Fahr","Celsius");
+  celsius = lower;
 
-  while (fahr <= upper)
-  {
-    celsius = (5.0/9.0) * (fahr-32.0);
-    printf("%3.0f \t %6.1f \n",fahr,celsius);
-    fahr = fahr + step;
+  printf("%3s \t %6s \n", "Celsius", "Fahr");
+
+  while (celsius <= upper) {
+    fahr = celsius * (9.0 / 5.0) + 32.0;
+    printf("%3.0f \t %6.1f \n", celsius, fahr);
+    celsius = celsius + step;
   }
 }
