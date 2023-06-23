43451a204af153029c4dcc0ae298051683924f52
Alex <ascott.sco@gmail.com>
2023-06-23 12:06:39 +0100

    Temperature converter w headings

--- tempconverter.c
+++ tempconverter.c
@@ -3,6 +3,30 @@
 /* print Fahrenheit-Celsius table
  * for fahr = 0, 20, ..., 300; 
  * using floating point arithmetic*/
+
+/*
+int main() 
+{
+  float fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  fahr = lower;
+  
+  printf("%3s \t %6s \n","Fahr","Celsius");
+
+  while (fahr <= upper)
+  {
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f \t %6.1f \n",fahr,celsius);
+    fahr = fahr + step;
+  }
+}
+*/
+
 int main() 
 {
   float fahr, celsius;
