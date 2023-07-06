baa1c15c340cb30c2c24a99fd7761b2b97484c9c
Alex <ascott.sco@gmail.com>
2023-07-06 22:19:49 +0100

    linecounter ex3

--- linecounter.c
+++ linecounter.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
 int main() {
-  int current_char, previous_char;
+  int c;
 
-  // Read the first character
-  current_char = getchar();
-
-  while (current_char != EOF) {
-    // Print the character if it's not a space or if it's the first space in a
-    // consecutive series
-    if (current_char != ' ' || previous_char != ' ') {
-      putchar(current_char);
-    }
-
-    // Store the current character as the previous character for the next
-    // iteration
-    previous_char = current_char;
-
-    // Read the next character
-    current_char = getchar();
+  while ((c = getchar()) != EOF) {
+    if (c == '\t') {
+      putchar('\\');
+      putchar('t');
+    };
+    if (c == '\b') {
+      putchar('\\');
+      putchar('b');
+    };
+    if (c == '\\') {
+      putchar('\\');
+      putchar('\\');
+    };
+    c = getchar();
   }
 
   return 0;
