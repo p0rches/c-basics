86d36131e0063ae2688911d5c6db5d6f1910b98e
Alex <ascott.sco@gmail.com>
2023-07-06 16:44:30 +0100

    linecounter ex1

--- linecounter.c
+++ linecounter.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
 int main() {
-  int c, nl;
+  int c, nl, tab, blank;
 
   nl = 0;
   while ((c = getchar()) != EOF) {
     if (c == '\n') {
       ++nl;
     };
+    if (c == ' ') {
+      ++blank;
+    };
+    if (c == '\t') {
+      ++tab;
+    };
   };
 
+  printf("Newlines: ");
+  printf("%d\n", nl);
+
+  printf("Tabs: ");
+  printf("%d\n", nl);
+
+  printf("Blanks: ");
   printf("%d\n", nl);
 }
