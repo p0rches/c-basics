7a03a3f7029fe8ad438635986498c60e0b5d20fe
Alex <ascott.sco@gmail.com>
2024-01-21 14:02:53 +0000

    Exercise 3-01

---
+++ Chapter3/bin_search_after.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define MAX_LENGTH 1000
+
+int binsearch(int x, int v[], int n);
+
+int main() {
+  int x = 9, v[MAX_LENGTH] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, n = 10;
+
+  int result = binsearch(x, v, n);
+
+  if (result != -1)
+    printf("The input %d, is found at position %d in the array.\n", x, result);
+  else
+    printf("The input %d, is not found in the array.\n", x);
+
+  return 0;
+}
+
+int binsearch(int x, int v[], int n) {
+  int low, high, mid;
+
+  low = 0;
+  high = n - 1;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if (x < v[mid])
+      high = mid - 1;
+    else
+      low = mid + 1;
+  }
+
+  if (x == v[low - 1]) {
+    return low - 1;
+  }
+
+  return -1;
+}
