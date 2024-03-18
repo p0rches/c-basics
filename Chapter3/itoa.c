63635a78896440346f09ad763f7ebe28540e678e
Alex <ascott.sco@gmail.com>
2024-03-18 12:33:38 +0000

    Formatting issue

--- Chapter3/itoa.c
+++ Chapter3/itoa.c
@@ -6,11 +6,10 @@
 void int_to_array(int n, char s[]);
 void str_rev(char str[], int index, int size);
 
-int main(void)
-{
+int main(void) {
   char str[MAX];
   int num = 12345789;
- 
+
   int_to_array(num, str);
 
   printf("%s\n", str);
@@ -26,13 +25,12 @@ void int_to_array(int n, char s[]) {
   i = 0;
   do {
     s[i++] = abs(n % 10) + '0';
-  } while ((n/= 10) > 0);
+  } while ((n /= 10) > 0);
 
   if (sign < 0)
     s[i++] = '-';
 
   s[i] = '\0';
-
 }
 
 void str_rev(char str[], int index, int size) {
@@ -42,9 +40,9 @@ void str_rev(char str[], int index, int size) {
   str[index] = str[size - index];
   str[size - index] = temp;
 
-  if (index == size/2){
+  if (index == size / 2) {
     return;
   }
 
-  str_rev(str, index+1, size);
+  str_rev(str, index + 1, size);
 }
